2c9828e261cf554c0ee33a7df7dd0e4914a9572a
twoelephant <1136742281@qq.com>
2022-08-26 11:55:04 +0800

    121

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { HashRouter } from "react-router-dom";
+import { Route, Routes } from "react-router";
+import Login from "./pages/login/login";
+import Home from "./pages/home/home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Login/>}/>
+        <Route path="/home" element={<Home/>}/>
+      </Routes>
+    </HashRouter>
   );
 }
 
