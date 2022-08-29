c71d681f920293230dd04938550aeadbf5d1a78d
aaa <aaa@example.com>
2022-08-29 16:07:43 +0800

    v0.0.1

--- src/pages/home/nav/Nav.js
+++ src/pages/home/nav/Nav.js
@@ -4,24 +4,11 @@ import {
 } from '@ant-design/icons';
 import { Menu } from 'antd';
 import React, { useState, useEffect } from 'react';
-
 import 'antd/dist/antd.min.css';
 import './Nav.scss'
 import { NavLink } from 'react-router-dom';
-
-// useEffect(() => {
-//     var id = 1
-//     let id1 = 2
-
-// })
 function getItem(label, key, icon, children, type) {
-    //  useEffect(() => {
-    //     var id = 1
-    //     let id1 = 2
-
-    // })
-
-
+  
     return {
         key,
         icon,
@@ -32,18 +19,6 @@ function getItem(label, key, icon, children, type) {
     };
 }
 
-
-// const items = [
-//     getItem(<NavLink to='/home'>监控</NavLink>, '1', <EyeOutlined />),
-//     getItem('消息通知', 'sub1', <BellOutlined />, [
-//         getItem(<NavLink to='/home/shop/:1'>1 号店</NavLink>, '5'),
-//         getItem(<NavLink to='/home/shop/:2'>2 号店</NavLink>, '6'),
-//         getItem('3 号店', '7'),
-//         getItem('4 号店', '8'),
-//     ]),
-
-// ];
-
 const Nav = (props) => {
     
     const [newItem1,setNewitem1] = useState([])
@@ -52,29 +27,17 @@ const Nav = (props) => {
         { id: 3, name: '2 号店' },
         { id: 4, name: '3 号店' },
     ]
-    // let newItem =
-    //     [
-    //         getItem(<NavLink to={'/home/shop/1'}>1 号店</NavLink>, '5'),
-    //         getItem(<NavLink to='/home/shop/2'>2 号店</NavLink>, '6'),
-    //         getItem('3 号店', '7'),
-    //         getItem('4 号店', '8'),
-    //     ]
+ 
     useEffect(() => {
-        // setId(1)
-        console.log(1)
 
         let newItem=[]
         for(let item of num ){
-            console.log(2)
-            console.log(item)
+           
             newItem.push(getItem(<NavLink to={'/home/shop/' + item.id}>{item.name}</NavLink>, item.id))
             
             setNewitem1(newItem)
         }
     },[])
-
-   
-
 
     const [collapsed,] = useState(false);
     return (
@@ -87,17 +50,11 @@ const Nav = (props) => {
                     mode="inline"
                     theme="dark"
                     inlineCollapsed={collapsed}
-                    // items={items}
                     items={[
                         getItem(<NavLink to='/home'>监控</NavLink>, '1', <EyeOutlined />),
                         getItem('消息通知', 'sub1', <BellOutlined />, newItem1),
                     ]}
 
-
-
-                // onClick={(value) => {
-                //     console.log(value.key)
-                // }}
                 />
 
 
